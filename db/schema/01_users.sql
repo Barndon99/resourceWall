5e35eb09c032fae0fa9165f00b57d393298c7e37
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-06-26 12:12:39 -0600

    Added database with Leam

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,58 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS resources CASCADE;
+DROP TABLE IF EXISTS users_resources CASCADE;
+DROP TABLE IF EXISTS resource_ratings CASCADE;
+DROP TABLE IF EXISTS resource_comments CASCADE;
+
+
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  profile_image VARCHAR(255),
+  timestamp TIMESTAMP
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  category_img VARCHAR(255) NOT NULL,
+  timestamp TIMESTAMP
+);
+
+CREATE TABLE resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  url VARCHAR(255) NOT NULL,
+  resource_img_url VARCHAR(255) NOT NULL,
+  timestamp TIMESTAMP
+);
+
+CREATE TABLE users_resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  timestamp TIMESTAMP
+);
+
+CREATE TABLE resource_ratings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  rating INTEGER NOT NUll,
+  timestamp TIMESTAMP
+);
+
+CREATE TABLE resource_comments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  comments VARCHAR(255) NOT NULL,
+  timestamp TIMESTAMP
 );
