d683212514e2451729a638abad65c28113723f8f
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-06-26 13:17:31 -0600

    Users and categories seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,11 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users(name, email, password, profile_image, timestamp)
+VALUES ('Greg Smith', 'gregsmith@gmail.com', 'password', 'https://img.favpng.com/23/3/6/circle-line-point-area-clip-art-png-favpng-CvaYhEcU64kWWSUQa3gVdNS78.jpg', current_timestamp),
+('Sarah Robertson', 'sarahrobertson@gmail.com', 'password', 'https://img.favpng.com/23/3/6/circle-line-point-area-clip-art-png-favpng-CvaYhEcU64kWWSUQa3gVdNS78.jpg', current_timestamp),
+('Bob Bobby', 'bobbobby@gmail.com', 'password', 'https://img.favpng.com/23/3/6/circle-line-point-area-clip-art-png-favpng-CvaYhEcU64kWWSUQa3gVdNS78.jpg', current_timestamp),
+('Cheryl Cherry', 'Cherylcherry@gmail.com', 'password', 'https://img.favpng.com/23/3/6/circle-line-point-area-clip-art-png-favpng-CvaYhEcU64kWWSUQa3gVdNS78.jpg', current_timestamp),
+('Greggory Greggson', 'greggorygreggson@gmail.com', 'password', 'https://img.favpng.com/23/3/6/circle-line-point-area-clip-art-png-favpng-CvaYhEcU64kWWSUQa3gVdNS78.jpg', current_timestamp);
+
+
+
+
---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories(name, category_img, timestamp)
+VALUES('Git', 'https://portswigger.net/cms/images/0d/77/1622-article-200415-git-body-text.png', current_timestamp),
+('HTML/CSS', 'https://divbyte.com/wp-content/uploads/2019/02/html-css.png', current_timestamp),
+('JavaScript', 'https://blog.logrocket.com/wp-content/uploads/2021/03/javascript-php-developers.png', current_timestamp)
