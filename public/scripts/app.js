42fb3eb1dcac5fa333a6fcaeb09c8798840731b5
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-06-26 20:57:31 -0600

    Fix routes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,11 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+//$(() => {
+//  $.ajax({
+//    method: "GET",
+//    url: "/api/categories"
+//  }).done((categories) => {
+//    for(category in categories) {
+//    $("<div>").text(category.name).appendTo($("body"));
+//    }
+//  });
+//});
+//
--- routes/widgets.js
+++ routes/widgets.js
@@ -5,23 +5,25 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
-
-module.exports = (db) => {
-  router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-  return router;
-};
+//
+//const express = require('express');
+//const router  = express.Router();
+//
+//module.exports = (db) => {
+//  router.get("/", (req, res) => {
+//    let query = `SELECT * FROM widgets`;
+//    console.log(query);
+//    db.query(query)
+//      .then(data => {
+//        const widgets = data.rows;
+//        res.json({ widgets });
+//      })
+//      .catch(err => {
+//        res
+//          .status(500)
+//          .json({ error: err.message });
+//      });
+//  });
+//  return router;
+//};
+//
