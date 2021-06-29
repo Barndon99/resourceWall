4fe49ee1295e9fba439bc0d7473e2bd68d1901ce
leam <leam.murphy1@gmail.com>
2021-06-29 15:48:57 +0000

    minor fix

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -65,5 +65,4 @@ CREATE TABLE resource_comments (
 \i db/seeds/03_resources.sql
 \i db/seeds/04_user_resources.sql
 \i db/seeds/06_resource_ratings.sql
-\i db/seeds/06_resource_comments.sql
 \i db/seeds/07_resource_comments.sql
