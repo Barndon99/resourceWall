1829081d6246183be2e429ed29448dc733959f8d
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-06-29 20:18:30 -0600

    Refactored user page ejs and route

--- routes/users_routes.js
+++ routes/users_routes.js
@@ -1,3 +1,4 @@
+const { promiseImpl } = require('ejs');
 const { response } = require('express');
 const express = require('express');
 const router  = express.Router();
@@ -8,41 +9,26 @@ const userRouter = (db) => {
   });
 
   router.get('/:id', (req, res) => {
-    const id = req.session.id;
-    db.query(`SELECT * FROM users WHERE users.id = ${req.params.id};`)
+    const id = req.params.id;
+    console.log("HERE");
+    Promise.all([db.query(`SELECT * FROM users WHERE users.id = ${id};`), db.query(`
+    SELECT resources.*, users.id as userId, resources.id as id
+    FROM resources
+    JOIN users ON resources.owner_id = users.id
+    WHERE resources.owner_id = ${id};
+    `), db.query(`
+    SELECT resources.*, users_resources.guest_id as guestId
+    FROM resources
+    JOIN users_resources ON users_resources.resource_id = resources.id
+    WHERE users_resources.guest_id = ${id};
+    `)])
       .then((response) => {
-        console.log(response.rows[0]);
-        const templateVars = { user: response.rows[0]};
+        const userInfo = response[0].rows[0];
+        const ownedResources = response[1].rows;
+        const savedResources = response[2].rows;
+        console.log("USER INFO: ", userInfo, "OWNED RESOURCES: ", ownedResources, "SAVED RESOURCES: ", savedResources);
+        const templateVars = { user: userInfo, owned: ownedResources, saved: savedResources};
         res.render('user_page', templateVars);
-      })
-  })
-
-  router.get("/:id/owned_resources", (req, res) => {
-    //users id will need to be a variable
-    const id = req.params.id;
-    console.log(id);
-    db.query(`
-      SELECT resources.*, users.id as userId, resources.id as id
-      FROM resources
-      JOIN users ON resources.owner_id = users.id
-      WHERE resources.owner_id = ${id};
-    `)
-      .then((response) => {
-        console.log(response.rows);
-        res.send(response.rows);
-      });
-  });
-
-  router.get("/:id/saved_resources", (req, res) => {
-    const id = req.params.id;
-    db.query(`
-      SELECT resources.*, users_resources.guest_id as guestId
-      FROM resources
-      JOIN users_resources ON users_resources.resource_id = resources.id
-      WHERE users_resources.guest_id = ${id};
-    `)
-      .then((response) => {
-        res.send(response.rows);
       })
   });
 
