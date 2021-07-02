1451179357f249963c5a60d867c562fa11620ca3
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-07-01 19:56:39 -0600

    IT WORKS

--- routes/resource_routes.js
+++ routes/resource_routes.js
@@ -30,17 +30,18 @@ const resourceRouter = (db) => {
       .then((response) => {
         const id = response.rows[0].id;
         console.log("MADE IT IN FIRST RESPONSE");
-        db.query(`INSERT INTO resources(category_id, owner_id, title, description, url, resource_img_url, timestamp)
-        VALUES(${id}, ${user_id}, '${title}', '${description}', '${url}', '${resourceImgUrl}', current_timestamp);`)
-        .then((response) => {
-          console.log("RESPONSE: ", response)
-          db.query(`SELECT * FROM resources WHERE resources.title = '${title}'`)
-          .then((response) => {
-            console.log(response);
-            const templateVars = {resource: response.rows, user: req.session.user_id}
-            console.log(templateVars);
-            res.render('resource_show', templateVars);
-          })
+        console.log("IMG URL: ", resourceImgUrl);
+        //, owner_id, title, description, url, resource_img_url, timestamp
+        //${user_id}, '${title}', '${description}', '${url}', '${resourceImgUrl}', current_timestamp
+        db.query(`INSERT INTO resources (owner_id, category_id, title, description, resource_img_url, url, timestamp)
+        VALUES (${user_id}, ${id}, '${title}', '${description}', '${resourceImgUrl}', '${url}', current_timestamp) RETURNING *;`)
+        .then((response2) => {
+          console.log("2nd QUERY", response2.rows);
+          const newId = response2.rows[0].id;
+          //const templateVars = {resource: response2.rows, user: req.session.user_id}
+          //console.log(templateVars);
+          //res.render('resource_show', templateVars);
+          res.redirect(302, `/resources/${newId}`);
         })
       })
   });
@@ -49,12 +50,17 @@ const resourceRouter = (db) => {
 
   // Change index to resource page
   router.get("/:id", (req, res) => {
+    console.log("******");
     const id = req.params.id;
-    db.query(`SELECT resources.*, resource_comments.comments as comments, users.name as name FROM resources JOIN resource_comments ON resource_comments.resource_id = resources.id JOIN users on resource_comments.guest_id = users.id WHERE resources.id = ${id} LIMIT 3;`)
+    //JOIN users on resource_comments.guest_id = users.id || users.name as name
+    db.query(`SELECT resources.*, resource_comments.comments as comments, users.name as name FROM resources LEFT JOIN resource_comments ON resources.id = resource_comments.resource_id LEFT JOIN users on resource_comments.guest_id = users.id WHERE resources.id = ${id} LIMIT 3;`)
     .then((response) => {
-      console.log(response.rows);
+      //console.log(response.rows);
       const id = req.session.user_id;
-      const templateVars = {resource: response.rows, user: id }
+      const resource = response.rows;
+      console.log(">>>>>XXXXXXX", response.rows[0]);
+      const templateVars = {resource, user: id }
+      //console.log("<<<<", templateVars);
       res.render('resource_show', templateVars);
     });
     //res.render('resource_page');
@@ -78,16 +84,16 @@ const resourceRouter = (db) => {
     const content = req.body.newComment;
     if (req.session.user_id) {
     db.query(`INSERT INTO resource_comments(guest_id, resource_id, comments, timestamp)
-      VALUES('${req.session.user_id}', '${id}', '${content}', current_timestamp);
+      VALUES('${req.session.user_id}', '${id}', '${content}', current_timestamp) RETURNING *;
       `)
       .then((response) => {
-      db.query(`SELECT resources.*, resource_comments.comments as comments, users.name as name FROM resources JOIN resource_comments ON resource_comments.resource_id = resources.id JOIN users on resource_comments.guest_id = users.id WHERE resources.id = ${id} LIMIT 3;`)
-        .then((response) => {
+      //db.query(`SELECT resources.*, resource_comments.comments as comments, users.name as name FROM resources JOIN resource_comments ON resource_comments.resource_id = resources.id JOIN users on resource_comments.guest_id = users.id WHERE resources.id = ${id} LIMIT 3;`)
+      //  .then((response) => {
 
           const templateVars = {resource: response.rows, user: req.session.user_id }
           console.log(templateVars.resource);
-          res.render('resource_show', templateVars);
-        })
+          res.redirect(302, `/resources/${id}`);
+     //   })
       });
     } else {
       res.redirect(302, '/login');
